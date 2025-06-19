617dbcc7b8541a2428d103bbf61259d7a2f56aee
xu11129 <3308170734@qq.com>
2025-06-19 19:13:44 +0800

    完成遍历省市县三级列表的功能。

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,17 @@ dependencies {
 
     implementation(libs.appcompat)
     implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation(fileTree("libs") { include("*.jar") })
+    testImplementation("junit:junit:4.12")
+    implementation("com.squareup.okhttp3:okhttp:3.4.1")
+    implementation("com.google.code.gson:gson:2.7")
+    implementation("com.github.bumptech.glide:glide:3.7.0")
+    implementation(files("libs/litepal-2.0.0.jar"))
+
+
 }
